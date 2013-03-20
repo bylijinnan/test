f50d6cb28c79629c43e9d645b8940722a8c5b0a6
bylijinnan <bylijinnan@gmail.com>
2013-03-20 14:22:57 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 //add sth
 //add in github.com
   public static void main(String[] args) {
-    System.out.println(12);kkk
+    System.out.println(12);
+    String s = "aa";
   }
 }
