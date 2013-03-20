4b9b30e6a78d6170e4e4dad523f40bbaeef9289b
bylijinnan <bylijinnan@gmail.com>
2013-03-20 14:26:10 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,10 +1,3 @@
 public class Hello {
-//本机输入中文？
-//github web page 输入中文
-//add sth
-//add in github.com
-  public static void main(String[] args) {
-    System.out.println(12);
-    String s = "aa";
-  }
+  //clear in github
 }
